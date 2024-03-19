b5b905b54ec190bf92d4f46f3e344989c56a48b2
thalissonvs <101291018+thalissonvs@users.noreply.github.com>
2024-03-19 23:34:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para aprofundar meus conhecimentos em desenvolvimento web.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Oi meu amor, me paga um lanche</h1>
+  return <h1>Oi meu amor, me paga um lanche</h1>;
 }
 
-export default Home
+export default Home;
